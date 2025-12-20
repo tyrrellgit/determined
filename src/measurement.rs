da34f02c86e6124f1c4f04ce39ab05bfefc7a63d
rory <rorytyrrell2@gmail.com>
2025-12-21 00:50:48 +0100

    simplify measurements

--- src/measurement.rs
+++ src/measurement.rs
@@ -6,41 +6,14 @@
 
 use crate::common::na as na;
 use crate::common::Epoch;
-use crate::state::State;
-
-/// A convenience alias for a column vector observation of length `O`.
-pub type Observation<O> = State<O>;
 
 #[derive(Clone, Debug)]
-pub struct Measurement<const S: usize, const O: usize, const N: usize> {
-    pub state: State<na::Const<S>>,                 // state vector (S x 1)
-    pub observation: State<na::Const<O>>,           // observation vector (O x 1)
-    pub covariance: na::SMatrix<f64, N, N>, // joint covariance (N x N), N should == S + O
-    pub jacobian: na::SMatrix<f64, O, S>,   // measurement jacobian (O x S)
-
-}
-
-// OMatrix allocator for <Dim, Dim> is not directly implemented
-
-impl<const S: usize, const O: usize, const N: usize> Measurement<S, O, N> {
-    pub fn new(state: State<na::Const<S>>, observation: State<na::Const<O>>) -> Self {
-        Measurement {
-            state,
-            observation,
-            covariance: na::SMatrix::<f64, N, N>::identity(),
-            jacobian: na::SMatrix::<f64, O, S>::zeros(),
-        }
-    }
-
-    /// TODO: Time-predict the measurement's state
-    pub fn predict(&self, epoch: &Epoch) -> State<na::Const<S>> {
-        let mut predicted = self.state.clone();
-        predicted.epoch = epoch.value;
-        predicted
-    }
-
-    /// TODO: Incorporate a new observation into this measurement
-    pub fn update(&mut self, observation: &State<na::Const<O>>) {
-        self.observation = observation.clone();
-    }
+pub struct Observation<T>
+where
+    T: na::Dim,
+    na::DefaultAllocator: na::base::allocator::Allocator<T, na::U1>
+        + na::allocator::Allocator<T, T>,
+{
+    pub value: na::Matrix<f64, T, na::U1, na::Owned<f64, T, na::U1>>,
+    pub epoch: Epoch,
 }
