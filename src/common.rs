fe6f7cea0397c75c37bd31f0a2f3bfa487c708c1
rory <rorytyrrell2@gmail.com>
2025-12-21 00:50:33 +0100

    add ops for Epoch

--- src/common.rs
+++ src/common.rs
@@ -3,10 +3,65 @@
 //! This module re-exports `nalgebra` as `na` for convenience and provides the
 //! `Algorithm` trait used by filter implementations.
 
+use std::ops::{Add, Sub};
+
 pub use nalgebra as na;
 pub use crate::composition::Composable;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Epoch {
-    pub value: u64,
+    pub value: i64, // signed to allow negative time
+}
+
+impl Epoch {
+    pub fn new(value: i64) -> Self {
+        Epoch { value }
+    }   
+}
+
+impl Default for Epoch {
+    fn default() -> Self {
+        Epoch { value: 0 }
+    }
+}
+
+impl Add for Epoch {
+    type Output = Epoch;
+
+    fn add(self, other: Epoch) -> Epoch {
+        Epoch {
+            value: self.value + other.value,
+        }
+    }
+}
+
+impl Add for &Epoch {
+    type Output = Epoch;
+
+    fn add(self, other: &Epoch) -> Epoch {
+        Epoch {
+            value: self.value + other.value,
+        }
+    }
+}
+
+impl Sub for Epoch {
+    type Output = Epoch;
+
+    fn sub(self, other: Epoch) -> Epoch {
+        Epoch {
+            value: self.value - other.value,
+        }
+    }
+}
+
+impl Sub for &Epoch {
+    type Output = Epoch;
+
+    fn sub(self, other: &Epoch) -> Epoch {
+        Epoch {
+            value: self.value - other.value,
+        }
+    }
 }
